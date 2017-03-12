9160fb7dc5f6190b0d9f47dee83ca2cc8be8c48d
Cliff <cliffsmith25@gmail.com>
2017-03-12 12:39:15 -0500

    Sound file not working

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,11 @@
       <div id="clock">
         <h1 v-on:click="timeToggle">{{ timeLeftFormatted }}</h1>
       </div>
+      <div id="type">
+        <h2>{{ timeType }}</h2>
+      </div>
+
+      <button v-on:click="alarmSound">Click me</button>
     </div>
   </div>
 </template>
@@ -24,34 +29,57 @@ export default {
   data () {
     return {
       timeFor: 'pom',
-      breakTime: 5,
-      pomTime: 20,
+      breakTime: 1,
+      pomTime: 1,
       timerRunning: false,
-      timerCode: ''
+      timerCode: '',
+      secondsElapsed: 0,
+      timeType: 'Pomodoro',
+      alarm: new Audio('./alarm.mp3')
     }
   },
   computed: {
     timeLeftFormatted: function () {
-      const min = this.timeLeft / 60
+      const min = Math.floor(this.timeLeft / 60)
       const sec = this.timeLeft % 60
       const trimSec = ('0' + sec.toString()).slice(-2)
       return min.toString() + ':' + trimSec
     },
     timeLeft: function () {
-      return this.pomTime * 60
+      if (this.timeType === 'Pomodoro') {
+        return this.pomTime * 60 - this.secondsElapsed
+      } else {
+        return this.breakTime * 60 - this.secondsElapsed
+      }
     }
   },
   methods: {
     incPom: function () {
+      if (this.timeType === 'Pomodoro' && this.timerRunning === true) {
+        this.timeToggle()
+        this.secondsElapsed = 0
+      }
       this.pomTime++
     },
     decPom: function () {
+      if (this.timeType === 'Pomodoro' && this.timerRunning === true) {
+        this.timeToggle()
+        this.secondsElapsed = 0
+      }
       this.pomTime--
     },
     incBreak: function () {
+      if (this.timeType === 'Break' && this.timerRunning === true) {
+        this.timeToggle()
+        this.secondsElapsed = 0
+      }
       this.breakTime++
     },
     decBreak: function () {
+      if (this.timeType === 'Break' && this.timerRunning === true) {
+        this.timeToggle()
+        this.secondsElapsed = 0
+      }
       this.breakTime--
     },
     timeToggle: function () {
@@ -60,10 +88,27 @@ export default {
         clearTimeout(this.timerCode)
       } else {
         this.timerRunning = true
+        this.timerCode = setInterval(this.timeTick, 1000)
       }
     },
     timeTick: function () {
-      'do something here'
+      this.secondsElapsed++
+      if (this.timeLeft === 0) {
+        this.timeToggle()
+        this.alarmSound()
+        this.secondsElapsed = 0
+        this.toggleTimeType()
+      }
+    },
+    toggleTimeType: function () {
+      if (this.timeType === 'Break') {
+        this.timeType = 'Pomodoro'
+      } else {
+        this.timeType = 'Break'
+      }
+    },
+    alarmSound: function () {
+      this.alarm.play()
     }
   }
 }
